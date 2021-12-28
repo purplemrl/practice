d8a5e04d42e0f0ed925921c6c2b4ce47f4a9bdb8
purplemrl <nikoo79m@gmail.com>
2021-12-28 23:56:44 +0330

    first

--- Task2/script13.js
+++ Task2/script13.js
@@ -1,31 +1,5 @@
-function show() {
-    document.getElementById('img1').style.display = 'inline-block';
-    document.getElementById('img2').style.display = 'inline-block';
-}
-
-function hide() {
-    document.getElementById('img1').style.display = 'none';
-    document.getElementById('img2').style.display = 'none';
-}
-
-function mouseover1() {
-    document.getElementById('img1').style.transform = 'rotate(180deg)';
-}
-
-function mouseover2() {
-    document.getElementById('img2').style.opacity = '100%';
-}
-
-function mouseout1() {
-    document.getElementById('img1').style.transform = 'rotate(360deg)';
-}
-
-function mouseout2() {
-    document.getElementById('img2').style.opacity = '50%';
-}
-
-function make() {
-    document.getElementById("name").innerHTML = fname.value + " " + lname.value;
-
-}
+$("btnShow").click(function () {
+    $("#img1").css("display", "inline-block");
+    $("#img2").css("display", "inline-block");
+})
 
