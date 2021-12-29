7ec3b2ecb97380e7417b7aa7a1b7528c71022aaa
purplemrl <nikoo79m@gmail.com>
2021-12-29 13:01:12 +0330

    first

--- Task2/script13.js
+++ Task2/script13.js
@@ -1,5 +1,38 @@
-$("btnShow").click(function () {
-    $("#img1").css("display", "inline-block");
-    $("#img2").css("display", "inline-block");
-})
+
+$(document).ready(function () {
+    $("#btnshow").click(function () {
+        $("#img1").css("display", "inline-block");
+        $("#img2").css("display", "inline-block");
+    });
+
+    $("#img1").mouseover(function () {
+        $("#img1").css("transform", "rotate(180deg");
+    });
+
+    $("#img1").mouseout(function () {
+        $("#img1").css("transform", "rotate(360deg)");
+    });
+
+    $("#img2").mouseover(function () {
+        $("#img2").css("opacity", "100%");
+    });
+
+    $("#img2").mouseout(function () {
+        $("#img2").css("opacity", "50%");
+    });
+
+    $("#btnhide").click(function () {
+        $("#img1").hide();
+        $("#img2").hide();
+    });
+
+    $("#btnsave").click(function () {
+        $("#name").text(fname.value + " " + lname.value);
+    });
+
+    $("#btnprint").click(function () {
+        window.print();
+    });
+});
+
 
